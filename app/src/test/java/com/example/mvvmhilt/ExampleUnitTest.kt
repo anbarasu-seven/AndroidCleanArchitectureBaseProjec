51f3b27be39b7eddbe783195b67b9f6246c05063
Anbarasu Chinna <anbarasu.seven@gmail.com>
2023-05-22 11:30:02 +0530

    Update ExampleUnitTest.kt
    sample unit test

--- app/src/test/java/com/example/mvvmhilt/ExampleUnitTest.kt
+++ app/src/test/java/com/example/mvvmhilt/ExampleUnitTest.kt
@@ -13,4 +13,39 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         Assert.assertEquals(4, 2 + 2)
     }
+    
+    @Test
+    public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
+        assertTrue(EmailValidator.isValidEmail("name@email.com"));
+    }
+
+    @Test
+    public void emailValidator_CorrectEmailSubDomain_ReturnsTrue() {
+        assertTrue(EmailValidator.isValidEmail("name@email.co.uk"));
+    }
+
+    @Test
+    public void emailValidator_InvalidEmailNoTld_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@email"));
+    }
+
+    @Test
+    public void emailValidator_InvalidEmailDoubleDot_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@email..com"));
+    }
+
+    @Test
+    public void emailValidator_InvalidEmailNoUsername_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("@email.com"));
+    }
+
+    @Test
+    public void emailValidator_EmptyString_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail(""));
+    }
+
+    @Test
+    public void emailValidator_NullEmail_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail(null));
+    }
 }
